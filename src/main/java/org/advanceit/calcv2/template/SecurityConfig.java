09bd343ddd55cc6c3653cf3c563a1ccbd00f82cf
Maksym Pyvovarov <mpyvovarov@advanceit.com>
2014-09-02 12:21:03 -0400

    Added security config

--- src/main/java/org/advanceit/calcv2/template/SecurityConfig.java
+++ src/main/java/org/advanceit/calcv2/template/SecurityConfig.java
@@ -6,12 +6,24 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
- 
+
+
+/**
+ * SecurityConfig class
+ * 
+ * <P>Configures Spring security
+ *    
+ * @author MPyvovarov
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
  
-	
+   /**
+    * Configures association between user credentials and roles
+    *
+    * @param       AuthenticationManagerBuilder auth
+    */
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 	  auth.inMemoryAuthentication().withUser("max").password("123456").roles("USER");
@@ -19,20 +31,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	  auth.inMemoryAuthentication().withUser("dba").password("123456").roles("DBA");
 	  auth.inMemoryAuthentication().withUser("sfdc").password("123456").roles("CALCV2_CLIENT");
 	}
- 
+     
+	/**
+     * Configures the access to the appropriate resources for the roles
+	 *
+	 * @param       HttpSecurity http
+	*/
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
  
 	  http.authorizeRequests()
-	  	//.antMatchers("/**").access("hasRole('USER')")
 	  	.antMatchers("/calcV2/**").access("hasRole('ROLE_CALCV2_CLIENT')")
 		.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
 		.antMatchers("/dba/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_DBA')")
 		.and().formLogin();
  
 	}
-	
-	//public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-		//do nothing
-	//}
 }
